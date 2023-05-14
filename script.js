3d3f135ddf008a303689dbdec6d7f02ead20e1b5
Suraj <121960253+SurajG20@users.noreply.github.com>
2023-05-14 19:24:37 +0530

    Fixed bugs

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const container = document.querySelector(".container");
 const rockbtn = document.getElementsByClassName("rock-button");
 const paperbtn = document.getElementsByClassName("paper-button");
 const scissorsbtn = document.getElementsByClassName("scissors-button");
-const results = document.querySelector('.results');
+const results = document.createElement('p');
 const csdisplay = document.getElementsByClassName("computer-score");
 const psdisplay = document.getElementsByClassName("player-score");
 
@@ -105,93 +105,3 @@ scissorsbtn[0].addEventListener("click", () => {
     chooseScissors();
     playRound();
 });
-
-
-
-// let playerScore = 0;
-// let computerScore = 0;
-
-// const choices = document.querySelectorAll('#rpc');
-// const results = document.querySelector('.results');
-// const restart = document.querySelector('#restart');
-// const score = document.querySelector('.score');
-// gameStart();
-// scoreUpdate();
-
-// function computerPlay() {
-//     let choice = Math.floor(Math.random() * (3) + 1);
-//     if (choice === 1) {
-//         return "rock";
-//     } else if (choice === 2) {
-//         return "paper";
-//     } else {
-//         return "scissors";
-//     }
-// }
-// function rpc(playerChoice, computerChoice) {
-//     playerChoice = playerChoice.toLowerCase();
-//     if (playerChoice === "rock" && computerChoice === "paper") {
-//         results.textContent = `You lost! ${computerChoice} beats ${playerChoice}!`;
-//         ++computerScore;
-//         scoreUpdate();
-//     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-//         results.textContent = `You lost! ${computerChoice} beats ${playerChoice}!`;
-//         ++computerScore;
-//         scoreUpdate();
-//     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-//         results.textContent = `You lost! ${computerChoice} beats ${playerChoice}!`;
-//         ++computerScore;
-//         scoreUpdate();
-//     } else if (playerChoice === computerChoice) {
-//         results.textContent = `It's a draw! ${computerChoice} can't beat ${playerChoice}!`;
-//     } else {
-//         results.textContent = `You won! ${playerChoice} beats ${computerChoice}`;
-//         ++playerScore;
-//         scoreUpdate();
-//     }
-// }
-
-// function gameStart() {
-//     playerScore = 0;
-//     computerScore = 0;
-//     choices.forEach(choice => {
-//         choice.disabled = false;
-//     });
-//     restart.style.visibility = 'hidden';
-//     restart.disabled = true;
-//     results.textContent = "";
-//     score.textContent = `${playerScore} - ${computerScore}`;
-// }
-
-// function gameEnd() {
-//     choices.forEach(choice => {
-//         choice.disabled = true;
-//     });
-//     restart.style.visibility = 'visible';
-//     restart.disabled = false;
-// }
-
-// function scoreUpdate() {
-//     if (playerScore == 5) {
-//         score.textContent = "You've won!!";
-//         gameEnd();
-//     } else if (computerScore == 5) {
-//         score.textContent = "You've Lost!!"
-//         gameEnd();
-//     } else {
-//         score.textContent = `${playerScore} - ${computerScore}`;
-//     }
-// }
-
-// function playRound(playerChoice) {
-//     let computerChoice = computerPlay();
-//     rpc(playerChoice, computerChoice)
-// }
-
-// choices.forEach(choice => {
-//     choice.addEventListener('click', () => {
-//         playRound(choice.textContent);
-//     })
-// });
-
-// restart.addEventListener('click', gameStart);
